f7e51e2ea711111b49cc466d54d1fa7da6b08131
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-06-30 03:15:05 +0100

    it bounces

--- pDots.py
+++ pDots.py
@@ -60,7 +60,7 @@ def main(stdscr):
         
     stdscr.getkey()
 
-def bounce(stdscr):
+def effect(stdscr):
     # Clear screen
     stdscr.clear()
 
@@ -118,15 +118,72 @@ def bounce(stdscr):
         stdscr.refresh()
         
     stdscr.getkey()
+
+def bounce(stdscr):
+    # Clear screen
+    stdscr.clear()
+
+    timeac = 0
+    now1=time.time()
+    startt = now1
+    interval=0.033
+
+    #workout the points to draw
+    #want about 4 bounces
+    # each bounce takes pi wich is 3.14, so terminal width 80 is a bounce 80/3.14 = 25.47, so a modifier on x of 4/25.47 = 0.16
+    #we want a height 80% of terminal of 24 which is 19.2, so y value multiplier of 19.2
+    #1 second should be about 5 x axis pixels, therefore time 5x converts time to x-axis value. x is in seconds
+    points = []
+    xval=0
+    xvalmodifier =  0.16
+    while xval < 80:
+
+        toadd = math.sin(xval*xvalmodifier)*19.2
+        #need to mirror in xaxis
+        
+        if toadd < 0:
+            toadd = toadd*-1
+    
+        toadd = 23 - toadd
+
+        points.append((xval,toadd))
+        xval+=1
+               
+    pointsC = 0
+    
+    while(True):
+        time2=time.time()
+        sleepm = interval-(time2-now1)
+        #stdscr.addstr(0,0,'sleeping {}'.format(sleepm))
+        sleep(sleepm)
+        now1=time.time()
+
+        timeac=timeac+interval
+
+        stdscr.clear()
+
+        stdscr.addstr(int(points[pointsC][1]),int(points[pointsC][0]), u'\u2585'.encode('UTF-8'))
+
+        pointsC+=1
+        if(len(points)<=pointsC):
+            pointsC=0
+
+        stdscr.refresh()
+        
+    stdscr.getkey()
     
 
 #parse arguments
 parser = argparse.ArgumentParser(description='Rainging Dots effect in terminal')
 parser.add_argument('--effect', dest='effect', action='store_true',
                help='should play option effect instead')
+parser.add_argument('--bounce', dest='bounce', action='store_true',
+               help='should play option effect instead')
 args = parser.parse_args()
  
 if(args.effect): 
+    wrapper(effect)
+elif(args.bounce):
     wrapper(bounce)
 else:
     wrapper(main)
